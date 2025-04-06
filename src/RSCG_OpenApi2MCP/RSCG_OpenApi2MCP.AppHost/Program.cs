51337a5e3f951a4123f82add3a286ded40419c7b
Andrei Ignat <ignatandrei@yahoo.com>
2025-04-06 17:24:20 +0300

    update

--- src/RSCG_OpenApi2MCP/RSCG_OpenApi2MCP.AppHost/Program.cs
+++ src/RSCG_OpenApi2MCP/RSCG_OpenApi2MCP.AppHost/Program.cs
@@ -7,9 +7,8 @@ var sut = builder.AddProject<Projects.OA2MCP_TestMe>("oa2mcp-testme");
 var inspector = builder
     .AddNpmApp("inspector-on-6274", "../MCPInspector", "OA2MCP_TestMe")
     .WaitFor(sut)
-    .WithEnvironment("SERVER_PORT", "5246")
-    //.WithHttpEndpoint(6274)
     .WithExternalHttpEndpoints()
+    .WithExplicitStart()
     ;
    
 builder.Build().Run();
