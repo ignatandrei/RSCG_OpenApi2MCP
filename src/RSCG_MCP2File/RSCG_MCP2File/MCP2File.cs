2ddb7717d517cf4a65850f9ea6d6d40451d14850
Andrei Ignat <ignatandrei@yahoo.com>
2025-11-30 22:15:57 +0200

    adding mcp

---
+++ src/RSCG_MCP2File/RSCG_MCP2File/MCP2File.cs
@@ -0,0 +1,50 @@
+﻿
+namespace RSCG_MCP2File;
+
+public class MCP2File : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context
+           .RegisterPostInitializationOutput(i =>
+           {
+               i.AddEmbeddedAttributeDefinition();
+               i.AddSource("MCPExportToFile.g.cs", @"
+                namespace MCP2File
+                {
+                    [global::Microsoft.CodeAnalysis.EmbeddedAttribute]
+                    internal class AddMCPExportToFile: global::System.Attribute {} 
+                }");
+           });
+
+        var mcpClasses= context.SyntaxProvider.ForAttributeWithMetadataName(
+            "MCP2File.AddMCPExportToFile",
+            static (node, _) => node is ClassDeclarationSyntax,
+            static (context, _) =>
+            {
+                INamedTypeSymbol? classSymbol = null;
+                var classDeclaration =context.TargetNode as ClassDeclarationSyntax;
+                if (classDeclaration is null)
+                    return classSymbol;
+
+                classSymbol = context.TargetSymbol as INamedTypeSymbol;
+                return classSymbol;
+            })
+             .Where(static m => m is not null)
+            .Select(static (m, _) => m!)
+            ;
+
+        context.RegisterSourceOutput(mcpClasses,(ctx,cls)=>
+        {
+            var classSymbol = cls;
+            var funcs = cls
+                    .GetMembers()
+                    .OfType<IMethodSymbol>()
+                    .Where(m => m.GetAttributes().Any(a => a.AttributeClass != null && a.AttributeClass.ToDisplayString().Contains("McpServerTool")))
+                    .ToArray();
+            ;
+        });
+
+
+    }
+}
